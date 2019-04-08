2ae3a93117835452fc4629e850c86137b3ef4d1a
Thomas Wenner <thmswenner@gmail.com>
2019-04-08 04:56:59 +0000

    Added simple request code

--- example.js
+++ example.js
@@ -1,3 +1,16 @@
 var https = require('https');
 
 console.log('I did it')
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+var callback = function () {
+  console.log('In response handle callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end()
